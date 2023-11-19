8fbbba882469abccd898e850e88d93713b3ab42c
Tomasz Budny <tomasz.budny.pro@gmail.com>
2023-11-19 18:08:10 +0100

    npm: add mongoose and validator

--- src/infra/repositories/users.js
+++ src/infra/repositories/users.js
@@ -1,37 +1,7 @@
-const { MongoClient } = require("mongodb")
+const { odm } = require('./service/odm')
 
-const url = 'mongodb://127.0.0.1:27017' // process.env.MONGODB_URL
-const client = new MongoClient(url)
-
-async function getUser() {
-
-    if (!client) {
-        console.log('No client')
-        console.log(client)
-        return
-    }
-
-    try {
-
-        const db = client.db('node1')
-
-        let collection = db.collection('users')
-
-        collection.insertOne({ name: 'Tomek', awesome: true })
-
-        let query = { name: 'Tomek' }
-
-        let res = await collection.findOne(query)
-
-        console.log(res)
-
-    } catch (err) {
-
-        console.log(err)
-    } finally {
-
-        client.close()
-    }
+function getUser() {
+    const User = odm.model('User', { name: String })
 }
 
 getUser()
