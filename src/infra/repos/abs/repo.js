9f62cb32241211d5fd1f37f24a2ec01ba0e8c9d5
Tomasz Budny <tomasz.budny.pro@gmail.com>
2023-11-26 22:33:54 +0100

    ref

--- src/infra/repos/abs/repo.js
+++ src/infra/repos/abs/repo.js
@@ -3,6 +3,7 @@ import { MongoClient } from 'mongodb'
 class Repo {
     collection(name) {
         this.client = new MongoClient(process.env.MONGODB_URL)
+
         return this.client.db(process.env.MONGODB_DB).collection(name)
     }
 }
