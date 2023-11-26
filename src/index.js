8a94e7a775ad16f918ad151cb3294ee850778aa9
Tomasz Budny <tomasz.budny.pro@gmail.com>
2023-11-26 18:44:01 +0100

    ref: index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const app = require('./app')
+import { listen } from './app'
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
+listen(port, () => {
     console.log('Server is up on port ' + port)
 })
